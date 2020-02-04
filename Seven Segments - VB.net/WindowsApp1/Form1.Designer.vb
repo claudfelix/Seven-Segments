b63d0279a2322442fafa5cda2d015cad3ea8741d
claudfelix <60541334+claudfelix@users.noreply.github.com>
2020-02-04 20:30:09 +0800

    Add files via upload

---
+++ Seven Segments - VB.net/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(399, 604)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(83, 154)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 138)
+        Me.Label1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(128, 302)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(165, 36)
+        Me.Label2.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(128, 112)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(165, 36)
+        Me.Label3.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(128, 494)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(165, 36)
+        Me.Label4.TabIndex = 4
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(301, 154)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(35, 138)
+        Me.Label5.TabIndex = 5
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.Color.White
+        Me.Label6.Location = New System.Drawing.Point(83, 345)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(35, 138)
+        Me.Label6.TabIndex = 6
+        '
+        'Label7
+        '
+        Me.Label7.BackColor = System.Drawing.Color.White
+        Me.Label7.Location = New System.Drawing.Point(301, 345)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(35, 138)
+        Me.Label7.TabIndex = 7
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 500
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(18, 22)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(26, 26)
+        Me.TextBox1.TabIndex = 8
+        Me.TextBox1.Text = "0"
+        Me.TextBox1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(422, 628)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "VB.net - Seven Segments"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Seven Segments - VB.net/WindowsApp1/Form1.vb
@@ -0,0 +1,104 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Enabled = True
+        Timer1.Interval = 500
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        TextBox1.Text += 1
+
+        If TextBox1.Text = 1 Then
+            Label1.BackColor = Color.Red
+            Label2.BackColor = Color.White
+            Label3.BackColor = Color.Red
+            Label4.BackColor = Color.Red
+            Label5.BackColor = Color.Red
+            Label6.BackColor = Color.Red
+            Label7.BackColor = Color.Red
+        End If
+        If TextBox1.Text = 2 Then
+            Label1.BackColor = Color.White
+            Label2.BackColor = Color.White
+            Label3.BackColor = Color.White
+            Label4.BackColor = Color.White
+            Label5.BackColor = Color.Red
+            Label6.BackColor = Color.White
+            Label7.BackColor = Color.Red
+        End If
+        If TextBox1.Text = 3 Then
+            Label1.BackColor = Color.White
+            Label2.BackColor = Color.Red
+            Label3.BackColor = Color.Red
+            Label4.BackColor = Color.Red
+            Label5.BackColor = Color.Red
+            Label6.BackColor = Color.Red
+            Label7.BackColor = Color.White
+        End If
+        If TextBox1.Text = 4 Then
+            Label1.BackColor = Color.White
+            Label2.BackColor = Color.Red
+            Label3.BackColor = Color.Red
+            Label4.BackColor = Color.Red
+            Label5.BackColor = Color.Red
+            Label6.BackColor = Color.White
+            Label7.BackColor = Color.Red
+        End If
+        If TextBox1.Text = 5 Then
+            Label1.BackColor = Color.Red
+            Label2.BackColor = Color.Red
+            Label3.BackColor = Color.White
+            Label4.BackColor = Color.White
+            Label5.BackColor = Color.Red
+            Label6.BackColor = Color.White
+            Label7.BackColor = Color.Red
+        End If
+        If TextBox1.Text = 6 Then
+            Label1.BackColor = Color.Red
+            Label2.BackColor = Color.Red
+            Label3.BackColor = Color.Red
+            Label4.BackColor = Color.Red
+            Label5.BackColor = Color.White
+            Label6.BackColor = Color.White
+            Label7.BackColor = Color.Red
+        End If
+        If TextBox1.Text = 7 Then
+            Label1.BackColor = Color.Red
+            Label2.BackColor = Color.Red
+            Label3.BackColor = Color.Red
+            Label4.BackColor = Color.Red
+            Label5.BackColor = Color.White
+            Label6.BackColor = Color.Red
+            Label7.BackColor = Color.Red
+        End If
+        If TextBox1.Text = 8 Then
+            Label1.BackColor = Color.White
+            Label2.BackColor = Color.White
+            Label3.BackColor = Color.Red
+            Label4.BackColor = Color.White
+            Label5.BackColor = Color.Red
+            Label6.BackColor = Color.White
+            Label7.BackColor = Color.Red
+        End If
+        If TextBox1.Text = 9 Then
+            Label1.BackColor = Color.Red
+            Label2.BackColor = Color.Red
+            Label3.BackColor = Color.Red
+            Label4.BackColor = Color.Red
+            Label5.BackColor = Color.Red
+            Label6.BackColor = Color.Red
+            Label7.BackColor = Color.Red
+        End If
+        If TextBox1.Text = 10 Then
+            Label1.BackColor = Color.Red
+            Label2.BackColor = Color.Red
+            Label3.BackColor = Color.Red
+            Label4.BackColor = Color.Red
+            Label5.BackColor = Color.Red
+            Label6.BackColor = Color.White
+            Label7.BackColor = Color.Red
+        End If
+        If TextBox1.Text > 10 Then
+            TextBox1.Text = 0
+        End If
+    End Sub
+End Class
